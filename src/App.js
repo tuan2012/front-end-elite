2d3c21d8f2db12d03e8d63c609e2ef257af5c932
tuan2012 <phampham1109@gmail.com>
2022-05-04 11:47:01 +0700

    change build project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './component/js/Login';
-import Home from './component/js/Home';
-import TopBar from './component/js/TopBar';
-import Footer from "./component/js/Footer";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
